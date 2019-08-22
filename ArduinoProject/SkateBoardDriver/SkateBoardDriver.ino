3508a1ac16206a7566d80567551084235b3858ba
guangze.song <songguangze@outlook.com>
2019-08-22 20:40:05 +0800

    first commit

---
+++ ArduinoProject/SkateBoardDriver/SkateBoardDriver.ino
@@ -0,0 +1,15 @@
+//#include "ArduinoJson.hpp"
+//todo? 消息是\0结尾，每次发送新行，换行符 \n
+
+
+void setup(){
+    Serial.begin(9600);
+}
+
+void loop(){
+     while (Serial.available())
+     {
+         
+     }
+
+}
