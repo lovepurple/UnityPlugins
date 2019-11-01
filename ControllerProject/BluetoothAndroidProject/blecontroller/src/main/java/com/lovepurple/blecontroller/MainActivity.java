40a616eb7de17bcd1a4c608f3610f6efa731d4d1
lovepurple <songguangze@outlook.com>
2019-11-02 02:14:18 +0800

    Base packages about Android BLE Library for Unity

---
+++ ControllerProject/BluetoothAndroidProject/blecontroller/src/main/java/com/lovepurple/blecontroller/MainActivity.java
@@ -0,0 +1,15 @@
+package com.lovepurple.blecontroller;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.unity3d.player.UnityPlayerActivity;
+
+public class MainActivity extends UnityPlayerActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
